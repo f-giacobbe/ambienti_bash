e2a980a15bb170cfdf7ec644ab405f591c405ae0
Francesco Giacobbe <100165496+f-giacobbe@users.noreply.github.com>
2026-01-04 19:50:19 +0100

    Traccia febbraio 2024

---
+++ tracce/24feb/rinomina_cartella.sh
@@ -0,0 +1,62 @@
+# Scrivere un programma shell che riceve da linea di comando tre argomenti, come segue:  RinominaCancella.sh    <cartella> <file>.   (Esempio:    RinominaCancella.sh  cartLibri    Libri.txt). 
+# Nel file passato come parametro ci sarà una serie di triple del tipo <nome_libro> <est1> <cartella_backup>. (Esempio: PiccoleDonne  cartRomanzi epub)
+# In pratica, per ogni tripla contenuta nel file passato come parametro, il programma prima cercherà nella cartella passato come parametro (cartLibri nell’esempio).   Nell’esempio (Esempio:    Cancella  B   cartfilm    est.txt), dovrà eliminare i file che iniziano con B dalla cartella cartFilm e che hanno l’estensione txt).  Prima di eliminarli però dovrà copiare questi file nella cartella backup, cambiando l’estensione in .bak (esempio, se il file si chiama pippo.txt diventerà pippo.bak).
+# Alla fine si stamperò in output, il numero di file eliminati per estensione.
+# Il programma deve gestire i casi d’eccezione (numero di argomenti diverso da tre, file già esistente, ecc.) interrompendo l’esecuzione con un messaggio all’utente.
+
+
+
+if [ $# -ne 2 ]; then
+    echo "Uso: $0 <cartella_sorgente> <file_istr>"
+    exit 1
+fi
+
+cartellaSorgente=$1
+fileInput=$2        # <nome_libro> <est> <cartella_backup>
+
+contenutoFile=$(cat "$fileInput")
+
+output="File cancellati per estensione:\n"
+estensioni=""
+count=0
+for w in "$contenutoFile"; do
+    if [ $count -eq 1 ]; then
+        estensioni=$(echo $w $estensioni)
+    else
+        let $count=($count+1)%3
+done
+
+for ext in "$estensioni"; do
+    fileExtCancellati=0
+
+    count=0
+    for w in "$contenutoFile"; do
+        if [ $count -eq 0 ]; then
+            nomeFileNoExt="$w"
+        elif [ $count -eq 1 ]; then
+            extFile="$w"
+        else
+            cartBackup="$w"
+
+            # Ho tutte le informazioni, se l'estensione coincide con ext posso iniziare
+            if [ "$ext" = "$extFile" ]; then
+                nomeCompletoFile="$nomeFileNoExt.$extFile"
+
+                # Esiste nella cartella sorgente?
+                if [ -f "$cartellaSorgente/$nomeCompletoFile" ]; then
+                    # Copio nella cartella di backup rinominandolo in .bak e lo elimino dalla cartella sorgente
+                    cp "$cartellaSorgente/$nomeCompletoFile" "$cartBackup/$nomeFileNoExt.bak"
+                    rm "$cartellaSorgente/$nomeCompletoFile"
+
+                    let fileExtCancellati=$fileExtCancellati+1
+                fi
+            fi
+        fi
+
+        let count=($count+1)%3
+    done
+
+    output=$(echo -e "$output\n$ext : $fileExtCancellati")
+done
+
+echo "$output"
